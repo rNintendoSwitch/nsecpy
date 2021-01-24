3e0606a46048ebfd1ad4c9a21535d0bff77fca56
Ian M <theianm2002@gmail.com>
2021-01-24 13:23:18 -0500

    Update to classes with region list

---
+++ scrape/classes.py
@@ -0,0 +1,19 @@
+import abc, typing
+
+
+class Game:
+    pass
+
+
+class ScrapeRegion(metaclass=abc.ABCMeta):
+    def __init__(self, cultureCode, familarName):
+        self.cultureCode = cultureCode
+        self.familarName = familarName
+
+    @abc.abstractmethod
+    def scrape(self) -> typing.Iterable[Game]:
+        pass
+
+
+class UndefinedRegion(metaclass=ScrapeRegion):
+    pass
--- scrape/noe.py
+++ scrape/noe.py
@@ -1,20 +1,10 @@
-from aiohttp import ClientSession
+from .classes import ScrapeRegion
 
-ENDPOINT = 'https://searching.nintendo-europe.com/{0}/select?q=*&start=0&rows=1000000&fq=(type:"game"AND((playable_on_txt%3A"HAC")))'
-REGIONS = [
-    'at',  # Austria - de_AT
-    'befr',  # Belgium / Belgique (French) - fr_BE
-    'benl',  # Belgium / België (Dutch) - nl_BE
-    'chde',  # Switzerland / Schweiz (German) - de_CH
-    'chfr',  # Switzerland / Suisse (French) - fr_CH
-    'chit',  # Switzerland / Svizzera (Italian) - it_CH
-    'de',  # Germany / Deutschland - de_DE
-    'en',  # UK & Ireland - en_GB
-    'es',  # Spain / España - es_ES
-    'fr',  # France - fr_FR
-    'it',  # Italy / Italia - it_IT
-    'nl',  # Netherlands / Nederland - nl_NL
-    'pt',  # Portugal - pt_PT
-    'ru',  # Russian - ru_RU
-    'za',  # South Africa - en_ZA
-]
+ENDPOINT = 'https://searching.nintendo-europe.com/{solrLanguage}/select?q=*&start=0&rows=1000000&fq=(type:"game"AND((playable_on_txt%3A"HAC")))'
+
+
+class EuropeanRegion(metaclass=ScrapeRegion):
+    def __init__(self, cultureCode, familarName, solrLanguage):
+        self.cultureCode = cultureCode
+        self.familarName = familarName
+        self.solrLanguage = solrLanguage
