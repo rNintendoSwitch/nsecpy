29bc643c1e114d15d0d5f4e29eab3ac4ab453002
Alexander Terry <alexterry43@gmail.com>
2021-02-20 11:32:41 -0900

    add timezones to regions

--- nsecpy/regions.py
+++ nsecpy/regions.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from .searchproviders.abcs import SearchProvider
 
@@ -9,27 +10,28 @@ class Region:
     familar_name: str
     search_provider: SearchProvider = None
     has_netinfo: bool = False  # https://www.nintendo.co.jp/netinfo/xx_XX/status.json
+    netinfo_TZ: Optional[str] = None
 
 
 # Regions from https://www.nintendo.com/regionselector/
 regions = [
     # -------- Americas --------
-    Region('en_US', 'USA', has_netinfo=True),
+    Region('en_US', 'USA', has_netinfo=True, netinfo_TZ="America/Los_Angeles"),
     Region('en_CA', 'Canada (English)'),
-    Region('fr_CA', 'Canada (French)', has_netinfo=True),
+    Region('fr_CA', 'Canada (French)', has_netinfo=True, netinfo_TZ="America/Toronto"),
     Region('es_LA', 'México'),
-    Region('pt_BR', 'Brasil', has_netinfo=True),
+    Region('pt_BR', 'Brasil', has_netinfo=True, netinfo_TZ="America/Los_Angeles"),
     Region('es_CO', 'Colombia'),
     Region('es_AR', 'Argentina'),
     Region('es_CL', 'Chile'),
     Region('es_PE', 'Perú'),
     # -------- Asia Pacific --------
-    Region('ja_JP', 'Japan', has_netinfo=True),
-    Region('ko_KR', 'Korea', has_netinfo=True),
-    Region('zh_CN', 'China Mainland (Tencent)', has_netinfo=True),
-    Region('zh_TW', 'Taiwan', has_netinfo=True),
+    Region('ja_JP', 'Japan', has_netinfo=True, netinfo_TZ="Asia/Tokyo"),
+    Region('ko_KR', 'Korea', has_netinfo=True, netinfo_TZ="asia/Seoul"),
+    Region('zh_CN', 'China Mainland (Tencent)', has_netinfo=True, netinfo_TZ="Asia/Shanghai"),
+    Region('zh_TW', 'Taiwan', has_netinfo=True, netinfo_TZ="Asia/Taipei"),
     Region('zh_CH', 'Hong Kong'),
-    Region('en_AU', 'Australia', has_netinfo=True),
+    Region('en_AU', 'Australia', has_netinfo=True, netinfo_TZ="Australia/Sydney"),
     Region('en_NZ', 'New Zealand'),
     # -------- Europe, Middle East & Africa --------
     Region('de_AT', 'Austria'),
@@ -37,21 +39,21 @@ regions = [
     Region('fr_BE', 'Belgique (French)'),
     Region('en_CZ', 'Czech Republic'),
     Region('en_DK', 'Denmark'),
-    Region('de_DE', 'Deutschland', has_netinfo=True),
-    Region('es_ES', 'España', has_netinfo=True),
+    Region('de_DE', 'Deutschland', has_netinfo=True, netinfo_TZ="Europe/Berlin"),
+    Region('es_ES', 'España', has_netinfo=True, netinfo_TZ="Europe/Madrid"),
     Region('en_FI', 'Finland'),
-    Region('fr_FR', 'France', has_netinfo=True),
+    Region('fr_FR', 'France', has_netinfo=True, netinfo_TZ="Europe/Paris"),
     Region('en_GR', 'Greece'),
     Region('en_HU', 'Hungary'),
     Region('he_IL', 'Israel'),
-    Region('it_IT', 'Italia', has_netinfo=True),
-    Region('nl_NL', 'Nederland', has_netinfo=True),
-    Region('pt_PT', 'Portugal', has_netinfo=True),
-    Region('ru_RU', 'Russia', has_netinfo=True),
+    Region('it_IT', 'Italia', has_netinfo=True, netinfo_TZ="Europe/Rome"),
+    Region('nl_NL', 'Nederland', has_netinfo=True, netinfo_TZ="Europe/Amsterdam"),
+    Region('pt_PT', 'Portugal', has_netinfo=True, netinfo_TZ="Europe/Lisbon"),
+    Region('ru_RU', 'Russia', has_netinfo=True, netinfo_TZ="Europe/Moscow"),
     Region('en_ZA', 'South Africa'),
     Region('en_SE', 'Sweden'),
     Region('de_CH', 'Schweiz (German)'),
     Region('fr_CH', 'Suisse (French)'),
     Region('it_CH', 'Svizzera (Italian)'),
-    Region('en_GB', 'UK & Ireland', has_netinfo=True),
+    Region('en_GB', 'UK & Ireland', has_netinfo=True, netinfo_TZ="Europe/London"),
 ]
