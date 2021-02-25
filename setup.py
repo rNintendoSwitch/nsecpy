ca21a29aeab01559da8cc99d8ca320c810ef436c
Ian M <theianm2002@gmail.com>
2021-02-24 22:20:59 -0500

    chore: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name='nsecpy',
     packages=find_packages(include=['nsecpy']),
-    version='0.0.0',
+    version='0.1.0',
     description='Python library that provides common interfaces to select Nintendo eShop APIs',
     author='7bitlyrus, mralext20',
     license='MIT',
