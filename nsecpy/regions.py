2ee25eb3eeb100df911abbdd53a535d084c8e86e
Ian M <theianm2002@gmail.com>
2021-03-07 14:00:43 -0500

    regions.py schema: add has listing, remove netinfo

--- nsecpy/regions.py
+++ nsecpy/regions.py
@@ -10,7 +10,7 @@ class Region:
     culture_code: str
     familar_name: str
     # search_provider: SearchProvider = None
-    has_netinfo: bool = False
+    supports_listing: bool = True
     netinfo_TZ: Optional[str] = None
 
     async def getStatus(self) -> Status:
@@ -24,22 +24,22 @@ class Region:
 # Regions from https://www.nintendo.com/regionselector/
 regions_list = [
     # -------- Americas --------
-    Region('en_US', 'USA', has_netinfo=True, netinfo_TZ="America/Los_Angeles"),
+    Region('en_US', 'USA', netinfo_TZ="America/Los_Angeles"),
     Region('en_CA', 'Canada (English)'),
-    Region('fr_CA', 'Canada (French)', has_netinfo=True, netinfo_TZ="America/Toronto"),
-    Region('es_LA', 'México'),
-    Region('pt_BR', 'Brasil', has_netinfo=True, netinfo_TZ="America/Los_Angeles"),
+    Region('fr_CA', 'Canada (French)', netinfo_TZ="America/Toronto"),
+    Region('es_MX', 'México', netinfo_TZ="America/Los_Angeles"),
+    Region('pt_BR', 'Brasil', netinfo_TZ="America/Los_Angeles"),
     Region('es_CO', 'Colombia'),
     Region('es_AR', 'Argentina'),
     Region('es_CL', 'Chile'),
     Region('es_PE', 'Perú'),
-    # -------- Asia Pacific --------
-    Region('ja_JP', 'Japan', has_netinfo=True, netinfo_TZ="Asia/Tokyo"),
-    Region('ko_KR', 'Korea', has_netinfo=True, netinfo_TZ="Asia/Seoul"),
-    Region('zh_CN', 'China Mainland (Tencent)', has_netinfo=True, netinfo_TZ="Asia/Shanghai"),
-    Region('zh_TW', 'Taiwan', has_netinfo=True, netinfo_TZ="Asia/Taipei"),
-    Region('zh_CH', 'Hong Kong'),
-    Region('en_AU', 'Australia', has_netinfo=True, netinfo_TZ="Australia/Sydney"),
+    # # -------- Asia Pacific --------
+    Region('ja_JP', 'Japan', netinfo_TZ="Asia/Tokyo"),
+    Region('ko_KR', 'Korea', netinfo_TZ="Asia/Seoul"),
+    Region('zh_CN', 'China Mainland (Tencent)', netinfo_TZ="Asia/Shanghai", supports_listing=False),
+    Region('zh_TW', 'Taiwan', netinfo_TZ="Asia/Taipei", supports_listing=False),
+    Region('zh_HK', 'Hong Kong'),
+    Region('en_AU', 'Australia', netinfo_TZ="Australia/Sydney"),
     Region('en_NZ', 'New Zealand'),
     # -------- Europe, Middle East & Africa --------
     Region('de_AT', 'Austria'),
@@ -47,23 +47,23 @@ regions_list = [
     Region('fr_BE', 'Belgique (French)'),
     Region('en_CZ', 'Czech Republic'),
     Region('en_DK', 'Denmark'),
-    Region('de_DE', 'Deutschland', has_netinfo=True, netinfo_TZ="Europe/Berlin"),
-    Region('es_ES', 'España', has_netinfo=True, netinfo_TZ="Europe/Madrid"),
+    Region('de_DE', 'Deutschland', netinfo_TZ="Europe/Berlin"),
+    Region('es_ES', 'España', netinfo_TZ="Europe/Madrid"),
     Region('en_FI', 'Finland'),
-    Region('fr_FR', 'France', has_netinfo=True, netinfo_TZ="Europe/Paris"),
+    Region('fr_FR', 'France', netinfo_TZ="Europe/Paris"),
     Region('en_GR', 'Greece'),
     Region('en_HU', 'Hungary'),
-    Region('he_IL', 'Israel'),
-    Region('it_IT', 'Italia', has_netinfo=True, netinfo_TZ="Europe/Rome"),
-    Region('nl_NL', 'Nederland', has_netinfo=True, netinfo_TZ="Europe/Amsterdam"),
-    Region('pt_PT', 'Portugal', has_netinfo=True, netinfo_TZ="Europe/Lisbon"),
-    Region('ru_RU', 'Russia', has_netinfo=True, netinfo_TZ="Europe/Moscow"),
+    Region('he_IL', 'Israel', supports_listing=False),
+    Region('it_IT', 'Italia', netinfo_TZ="Europe/Rome"),
+    Region('nl_NL', 'Nederland', netinfo_TZ="Europe/Amsterdam"),
+    Region('pt_PT', 'Portugal', netinfo_TZ="Europe/Lisbon"),
+    Region('ru_RU', 'Russia', netinfo_TZ="Europe/Moscow"),
     Region('en_ZA', 'South Africa'),
     Region('en_SE', 'Sweden'),
     Region('de_CH', 'Schweiz (German)'),
     Region('fr_CH', 'Suisse (French)'),
     Region('it_CH', 'Svizzera (Italian)'),
-    Region('en_GB', 'UK & Ireland', has_netinfo=True, netinfo_TZ="Europe/London"),
+    Region('en_GB', 'UK & Ireland', netinfo_TZ="Europe/London"),
 ]
 
 # Comprehend list into dict, indexed by culture code
