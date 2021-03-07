1bb492d5819400b58037989363da62b7758d1873
Ian M <theianm2002@gmail.com>
2021-03-07 12:27:46 -0500

    Implement listing class constructors

--- nsecpy/listing.py
+++ nsecpy/listing.py
@@ -3,6 +3,7 @@ from datetime import datetime  # for typehinting
 from typing import TYPE_CHECKING, Generator, List, Literal, Optional
 
 import aiohttp
+import dateparser
 
 
 if TYPE_CHECKING:
@@ -10,12 +11,21 @@ if TYPE_CHECKING:
 
 
 @dataclass
-class ContentType:
+class RatingContent:
     id: int = None
     name: str = None
     type: Literal["descriptor", "interactive"] = None
     image_url: Optional[str] = None  # JP Field
     svg_image_url: Optional[str] = None  # JP Field
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.type = data['type']
+        if data.get('image_url'):
+            self.image_url = data['image_url']
+        if data.get('svg_image_url'):
+            self.svg_image_url = data['svg_image_url']
 
 
 @dataclass
@@ -23,9 +33,16 @@ class Rating:
     age: int = None
     id: int = None
     image_url: str = None
-    name: str = None  # Literal["M", "T","10","e"] ??? expand and replace hint
+    name: str = None  # Literal["M", "T","E10","E"] ??? expand and replace hint
     provisional: bool = None
     svg_image_url: str = None
+
+    def __init__(self, data):
+        self.age = data['age']
+        self.id = data['id']
+        self.image_url = data['image_url']
+        self.provisional = data['provisional']
+        self.svg_image_url = data['svg_image_url']
 
 
 @dataclass
@@ -33,17 +50,21 @@ class RatingSystem:
     id: int = None
     name: Literal["PEGI", "ESRB", "CERO"] = None
 
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+
 
 @dataclass
 class Game:
-    content_type: str = None
+    content_type: str = None  # Literal["game", "bundle"] ??? expand and replace hint
     dominant_colors: List[str] = None
     formal_name: str = None
     hero_banner_url: str = None
-    id: str = None
+    id: int = None
     is_new: bool = None
     public_status: Literal["public"] = None
-    rating_content: List[ContentType] = field(default_factory=list)
+    rating_content: List[RatingContent] = field(default_factory=list)
     rating: Rating = None
     rating_system: RatingSystem = None
     release_date_on_eshop: datetime = None
@@ -51,8 +72,25 @@ class Game:
     tags: List = field(default_factory=list)
     target_titles: List = field(default_factory=list)
 
+    def __init__(self, data):
+        self.content_type = data['content_type']
+        self.dominant_colors = data['dominant_colors']
+        self.formal_name = data['formal_name']
+        self.hero_banner_url = data['hero_banner_url']
+        self.id = data['id']
+        self.is_new = data['is_new']
+        self.public_status = data['public_status']
+        self.rating_content = [RatingContent(c) for c in data['rating_info']['content_descriptors']]
+        self.rating = Rating(data['rating_info']['rating'])
+        self.rating_system = RatingSystem(data['rating_info']['rating_system'])
+        # TODO: is this dateparser correct?
+        self.release_date_on_eshop = dateparser.parse(data['release_date_on_eshop'], settings={'TIMEZONE': "UTC"})
+        self.screenshots = [s['images'][0]['url'] for s in data['screenshots']]
+        self.tags = data['tags']
+        self.target_titles = data['target_titles']
 
-async def fetchListing(region: "Region", type: Literal["sales", "new", "ranking"]) -> Generator[List[Game], None, None]:
+
+async def gameListing(region: "Region", type: Literal["sales", "new", "ranking"]) -> Generator[List[Game], None, None]:
     COUNT = 30
 
     # TODO: Check if (all) regions support this endpoint?
@@ -72,7 +110,7 @@ async def fetchListing(region: "Region", type: Literal["sales", "new", "ranking"
                 data = await request.json()
 
                 for game in data['contents']:
-                    yield game  # TODO: Parse
+                    yield Game(game)
 
                 if (offset + COUNT) >= data['total']:
                     break
