0351b78c54b34bd2127efe3413057e49b83e6687
Ian M <theianm2002@gmail.com>
2021-03-07 14:02:36 -0500

    Status: Update check if region supports netinfo

--- nsecpy/status.py
+++ nsecpy/status.py
@@ -90,8 +90,9 @@ class Status:
 
 
 async def getStatus(region: "Region") -> Status:
-    if not region.has_netinfo:
-        raise ValueError("region has no netinfo")
+    if not region.netinfo_TZ:
+        raise ValueError("Region does not support netinfo")
+
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://www.nintendo.co.jp/netinfo/{region.culture_code}/status.json") as request:
             request.raise_for_status()
